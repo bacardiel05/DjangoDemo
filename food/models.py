727af6acb6e1242cc56134a2944124d0d633e445
Brian <bacardiel@miners.utep.edu>
2020-07-02 23:18:44 -0600

    update on models

--- food/models.py
+++ food/models.py
@@ -12,3 +12,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=600, default="https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg")
+    #https://media.gettyimages.com/photos/sliced-pizza-on-chopping-board-overhead-view-picture-id763156339?s=2048x2048
--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,9 @@
 from . import views
 from django.urls import path
-
+app_name = 'food' # this allows to call path on index
 urlpatterns = [
     path('',views.index,name='index'),
     path('item/',views.item,name='item'),
-    #/food/1
+    #  /food/1
     path('<int:item_id>/',views.detail,name='detail'),
 ]
